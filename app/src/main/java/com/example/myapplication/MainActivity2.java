8566d8518061dac37757fd9731f3bed28d373f82
Andres Erazo <erazoandres14@gmail.com>
2022-12-10 03:37:42 -0500

    Interfaz 3 empezada

--- app/src/main/java/com/example/myapplication/MainActivity2.java
+++ app/src/main/java/com/example/myapplication/MainActivity2.java
@@ -6,20 +6,59 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 public class MainActivity2 extends AppCompatActivity {
 
-    private Button btn2 ;
-    private TextView title;
+    private Button btn1  , btn2 , btn3 ;
+    private TextView t1 , t2 , t3 ;
+    private ImageView i1, i2, i3;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
 
+        btn1 = findViewById(R.id.buttonList);
+        btn2 = findViewById(R.id.buttonList2);
+        btn2 = findViewById(R.id.buttonList3);
+
+        t1 = (TextView) findViewById(R.id.textView1);
+        t2 = (TextView) findViewById(R.id.textView2);
+        t3 = (TextView) findViewById(R.id.textView3);
+
+        i1 = (ImageView) findViewById(R.id.imageView1);
+        i2 = (ImageView) findViewById(R.id.imageView2);
+        i3 = (ImageView) findViewById(R.id.imageView3);
 
 
 
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent int1 = new Intent(getApplicationContext() , MainActivity3.class);
+                int1.putExtra("title",t1.getText().toString());
+                int1.putExtra("img",R.drawable.i1);
+                startActivity(int1);
+            }
+        });
+
+        btn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent int2 = new Intent(getApplicationContext() , MainActivity3.class);
+                startActivity(int2);
+            }
+        });
+
+        btn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent int3 = new Intent(getApplicationContext() , MainActivity3.class);
+                startActivity(int3);
+            }
+        });
 
 
     }
--- app/src/main/java/com/example/myapplication/MainActivity3.java
+++ app/src/main/java/com/example/myapplication/MainActivity3.java
@@ -2,13 +2,41 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MainActivity3 extends AppCompatActivity {
+
+    private ImageView img;
+    private TextView txt;
+    private Button btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main3);
+
+        img = findViewById(R.id.imageProduct);
+        txt = findViewById(R.id.textProduct);
+        btn = findViewById(R.id.buttonListPresentation);
+
+        Intent bund = getIntent();
+        img.setImageResource(bund.getIntExtra("img",0));
+        txt.setText(bund.getStringExtra("title"));
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(),MainActivity2.class);
+                startActivity(intent);
+            }
+        });
+
+
+
     }
 }
